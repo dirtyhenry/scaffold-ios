a3a8e7b4e5d7cc64032286dd17f4a3d76b0a03d3
Mickaël Floc’hlay <mickael.flochlay@margo.com>
2019-11-25 08:27:11 -0500

    feat(login-mvc): update storyboard with basic login form

--- scaffold/ViewController.swift
+++ scaffold/ViewController.swift
@@ -9,12 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    @IBOutlet weak var login: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
