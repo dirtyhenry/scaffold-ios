da6a8a6ee0a7236cf0f7cdacd7a55709aa3c0c6c
Mickaël Floc’hlay <mickael.flochlay@margo.com>
2019-11-25 07:55:43 -0500

    feat: initial commit
    As created by Xcode 11.0

---
+++ scaffold/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  scaffold
+//
+//  Created by Mickaël Floc'hlay on 25/11/2019.
+//  Copyright © 2019 mickf.net. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
